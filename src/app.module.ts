acf73a68d546e7bcd5d6fdb226c344480ebdac7d
Wassim Arfa <wassim.arfa@gmail.com>
2020-11-22 22:52:44 +0100

    student Entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Lesson } from './lesson/lesson.entity';
 import { LessonModule } from './lesson/lesson.module';
 import { StudentModule } from './student/student.module';
+import { Student } from './student/student.entity';
 @Module({
   imports: [
     TypeOrmModule.forRoot({
@@ -12,7 +13,8 @@ import { StudentModule } from './student/student.module';
       synchronize: true,
       useUnifiedTopology: true,
       entities: [
-        Lesson
+        Lesson,
+        Student
       ]
     }),
     GraphQLModule.forRoot({
