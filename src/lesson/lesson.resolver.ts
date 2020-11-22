0f6ab3c6509a988a71fdb73b181ce4dba05adfaf
Wassim Arfa <wassim.arfa@gmail.com>
2020-11-22 13:54:25 +0100

    lesson graphql resolver

---
+++ src/lesson/lesson.resolver.ts
@@ -0,0 +1,17 @@
+import { Resolver, Query } from "@nestjs/graphql";
+import { LessonType } from './lesson.type';
+
+@Resolver(of => LessonType)
+export class LessonResolver 
+{
+    @Query(returns => LessonType)
+    lesson() 
+    {
+        return {
+            id: '123456789',
+            name: 'Math Class',
+            startDate: (new Date()).toISOString(),
+            endDate: (new Date()).toISOString()
+        };
+    }
+}
